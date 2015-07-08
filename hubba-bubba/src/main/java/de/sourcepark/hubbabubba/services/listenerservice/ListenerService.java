93239afd0dfbcd1bef6912d073baf74674710476
Stefan Matyba <stefan.matyba@sourcepark.de>
2015-07-08 16:20:03 +0200

    Added empty ListenerService class that will later on be used to register/unregister listeners

---
+++ hubba-bubba/src/main/java/de/sourcepark/hubbabubba/services/listenerservice/ListenerService.java
@@ -0,0 +1,18 @@
+package de.sourcepark.hubbabubba.services.listenerservice;
+
+import de.sourcepark.hubbabubba.services.CandyService;
+import de.sourcepark.hubbabubba.services.RouteMap;
+
+/**
+ * The ListenerService class is used to register/unregister listeners.
+ * @author smatyba
+ */
+public class ListenerService extends CandyService {
+
+    @Override
+    public RouteMap getRoutes() {
+    }
+    
+    
+    
+}
