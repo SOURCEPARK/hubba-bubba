2a3dace259b78b03106f0ccfb4194352246354e1
Christian Jelinski <cjelinski@sourcepark.de>
2015-11-06 14:46:26 +0100

    added get and set methods for domain classes

--- hubba-bubba/src/main/java/de/sourcepark/hubbabubba/domain/Example.java
+++ hubba-bubba/src/main/java/de/sourcepark/hubbabubba/domain/Example.java
@@ -13,4 +13,8 @@ public class Example {
     private String exampleString = "Example String";
     private int exampleInt = 42;
     private List<String> exampleList = new ArrayList<String>() {{ add("Example"); }};
+    
+    public void setExampleString(String exampleString){
+        this.exampleString = exampleString;
+    }
 }
--- hubba-bubba/src/main/java/de/sourcepark/hubbabubba/domain/ListenerRegistration.java
+++ hubba-bubba/src/main/java/de/sourcepark/hubbabubba/domain/ListenerRegistration.java
@@ -4,19 +4,30 @@ import lombok.Data;
 
 /**
  * This object is used to register a listener with HubbaBubba
+ *
  * @author smatyba
  */
 @Data()
 public class ListenerRegistration {
+
     private String listenerName;
     private String listenerURL;
-    
+
     public ListenerRegistration(final String name, final String url) {
         this.listenerName = name;
         this.listenerURL = url;
     }
-    
+
     public ListenerRegistration() {
         this("", "");
+    }
+
+    public String getListenerURL() {
+        return listenerURL;
+    }
+
+    public String getListenerName() {
+
+        return listenerName;
     }
 }
