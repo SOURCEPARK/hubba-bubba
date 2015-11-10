7e61be65d5b038db1b79b21f935ce3113a7f02ef
Christian Jelinski <cjelinski@sourcepark.de>
2015-11-10 17:03:42 +0100

    added setter and getter

--- hubba-bubba/src/main/java/de/sourcepark/hubbabubba/domain/CandyError.java
+++ hubba-bubba/src/main/java/de/sourcepark/hubbabubba/domain/CandyError.java
@@ -42,4 +42,46 @@ public class CandyError {
     public CandyError() {
         this(0, "ERROR", "");
     }
+
+    /**
+     * @return the errorCode
+     */
+    public int getErrorCode() {
+        return errorCode;
+    }
+
+    /**
+     * @param errorCode the errorCode to set
+     */
+    public void setErrorCode(int errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    /**
+     * @return the errorName
+     */
+    public String getErrorName() {
+        return errorName;
+    }
+
+    /**
+     * @param errorName the errorName to set
+     */
+    public void setErrorName(String errorName) {
+        this.errorName = errorName;
+    }
+
+    /**
+     * @return the errorMessage
+     */
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    /**
+     * @param errorMessage the errorMessage to set
+     */
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
 }
